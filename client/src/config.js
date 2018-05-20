f21f0be211cc78810dcbd1b2f37cf936d962ee2c
dcalsky <dcalsky@163.com>
2018-05-20 21:07:23 +0800

    feat(client): upgrade webpack and babel

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export default {
-  URL: process.env.PRODUCTION ? 'yourdomain.com' : "http://localhost:3000"
+  URL: process.env.NODE_ENV === 'development' ? "http://localhost:3000" : 'yourdomain.com'
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,46 +1,29 @@
-var webpack = require('webpack')
-var path = require('path')
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    entry: {
-        app: ['./src/main.js'],
-        vendor: ['react', 'react-dom', 'react-router']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist',
-        filename: 'main.js'
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: 'node_moudles',
-                loader: 'babel-loader'
-            }, {
-                test: /\.css$/,
-                loader: 'css-loader!style-loader'
-            }, {
-                test: /\.json$/,
-                loader: 'json-loader'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.DefinePlugin({
-        'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'vendor.js'
-        })
-    ],
-    devServer: {
-        contentBase: './',
-        hot: true
-    }
-}
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?)$/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development")
+      }
+    })
+  ]
+};
